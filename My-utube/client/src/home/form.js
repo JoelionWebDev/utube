1c70f19d94e324f5c8d7d7a310791a0bdbd4eb3b
Igono Joel <igonojoel1010@gmail.com>
2023-05-08 22:12:05 +0100

    work now

--- My-utube/client/src/home/form.js
+++ My-utube/client/src/home/form.js
@@ -19,7 +19,7 @@ function Form() {
   const addData = async (e) => {
     e.preventDefault();
     const { name, email } = inputValue;
-    const res = await fetch("https://localhost:5001", {
+    const res = await fetch("https://joetechoffical.onrender.com/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
